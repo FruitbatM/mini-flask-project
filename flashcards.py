c884f3f93d2e4f15ec6c36906fd1fcf706ca2ca3
FruitbatM <mar.pavlovic@gmail.com>
2021-05-14 20:36:55 +0000

    Add count viewes function

--- flashcards.py
+++ flashcards.py
@@ -12,3 +12,13 @@ def welcome():
 @app.route("/date")
 def date():
     return "This page was served at " + str(datetime.now())
+
+
+# Add a page that shows how many times it has been viewed
+counter = 0
+
+@app.route("/count_views")
+def count_views():
+    global counter
+    counter += 1
+    return "This page was served " +str(counter) + " times"
