85afb00e7af8c46e6c3128c71a6d3363f01f3b06
FruitbatM <mar.pavlovic@gmail.com>
2021-05-14 20:26:30 +0000

    Create date function

---
+++ flashcards.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return "Welcome to my Flash Cards application!"
+
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
