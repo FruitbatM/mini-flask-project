d8575f676aa694c6a9f1c1e523fe16589fd823cf
FruitbatM <mar.pavlovic@gmail.com>
2021-05-15 18:34:48 +0000

    Add json flashcards json file

--- flashcards.py
+++ flashcards.py
@@ -1,25 +1,9 @@
-from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
-    return "Welcome to my Flash Cards application!"
-
-
-@app.route("/date")
-def date():
-    return "This page was served at " + str(datetime.now())
-
-
-# Add a page that shows how many times it has been viewed
-counter = 0
-
-@app.route("/count_views")
-def count_views():
-    global counter
-    counter += 1
-    return "This page was served " +str(counter) + " times"
+    return render_template("welcome.html")
 
